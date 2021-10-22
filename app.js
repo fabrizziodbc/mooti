901466b27f32e45495acb0690278a8cac8e435d0
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2021-10-21 19:50:09 -0500

    formulario

--- app.js
+++ app.js
@@ -1,35 +1,31 @@
 const loader = document.querySelector(".loader-container");
 const header__hamburguer = document.querySelector(".header__nav-hamburguer");
 const menu_full = document.querySelector(".menu-drop");
-const button = document.getElementById("contact__button");
+const form = document.getElementById("myForm");
 
 const YOUR_SERVICE_ID = "service_y5leog6";
 const YOUR_TEMPLATE_ID = "template_btrzvpb"
 const sendForm = (event) =>{
   event.preventDefault()
-  /* emailjs.sendForm(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, '#myForm')
+  emailjs.sendForm(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, '#myForm')
     .then(function(response) {
        console.log('SUCCESS!', response.status, response.text);
     }, function(error) {
        console.log('FAILED...', error);
-    }); */
-    alert("Muuu")
+    });
+    alert("Mensaje enviado");
 }
 
 
 addEventListener("DOMContentLoaded", () => {
-  window.addEventListener("load", () => {
-    /* loader.classList.toggle("show"); */
-    loader.style.opacity = 0;
-    loader.style.visibility = "hidden";
-  })
+
   header__hamburguer.addEventListener("click", () => {
     menu_full.classList.toggle("showRight");
   });
   menu_full.addEventListener("click", () => {
     menu_full.classList.toggle("showRight");
   });
-  button.addEventListener("click", sendForm)
+  form.addEventListener("submit", sendForm);
 });
 
 var slide = document.getElementsByClassName("slide");
