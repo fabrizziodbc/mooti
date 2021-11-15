448e0f4dae377ec76c97830ea740582307f505cd
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2021-11-15 08:26:18 -0500

    cambiando vista: qué es mooti

--- app.js
+++ app.js
@@ -3,22 +3,41 @@ const header__hamburguer = document.querySelector(".header__nav-hamburguer");
 const menu_full = document.querySelector(".menu-drop");
 const form = document.getElementById("myForm");
 
-const YOUR_SERVICE_ID = "service_y5leog6";
-const YOUR_TEMPLATE_ID = "template_btrzvpb"
-const sendForm = (event) =>{
-  event.preventDefault()
-  emailjs.sendForm(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, '#myForm')
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
-    alert("Mensaje enviado");
-}
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const element = entry.target.querySelector(".what__title-container");
+    const element2 = entry.target.querySelector(".what__img");
 
+    if (entry.isIntersecting) {
+      element.classList.add("animation_bt");
+      element2.classList.add("animation_bt");
+      return; // if we added the class, exit the function
+    }
+
+    // We're not intersecting, so remove the class!
+    element.classList.remove("animation_bt");
+    element2.classList.remove("animation_bt");
+  });
+});
+
+observer.observe(document.querySelector(".what"));
+
+const YOUR_SERVICE_ID = "service_y5leog6";
+const YOUR_TEMPLATE_ID = "template_btrzvpb";
+const sendForm = (event) => {
+  event.preventDefault();
+  emailjs.sendForm(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, "#myForm").then(
+    function (response) {
+      console.log("SUCCESS!", response.status, response.text);
+    },
+    function (error) {
+      console.log("FAILED...", error);
+    }
+  );
+  alert("Mensaje enviado");
+};
 
 addEventListener("DOMContentLoaded", () => {
-
   header__hamburguer.addEventListener("click", () => {
     menu_full.classList.toggle("showRight");
   });
@@ -29,83 +48,78 @@ addEventListener("DOMContentLoaded", () => {
 });
 
 var slide = document.getElementsByClassName("slide");
-        var indicator = document.getElementById("indicator");
-        var dots = document.getElementsByClassName("dots");
-        var autoplay = document.getElementsByClassName("container")[0].getAttribute("data-autoplay");
-        var l = slide.length;
-        var interval = 5000;
-        var set;
+var indicator = document.getElementById("indicator");
+var dots = document.getElementsByClassName("dots");
+var autoplay = document
+  .getElementsByClassName("container")[0]
+  .getAttribute("data-autoplay");
+var l = slide.length;
+var interval = 5000;
+var set;
 
-        window.onload = function () {
-            initialize();
-            slide[0].style.opacity = "1";
-            for (var j = 0; j < l; j++) {
-                indicator.innerHTML += "<div class='dots' onclick=change(" + j + ")></div>";
-            }
+window.onload = function () {
+  initialize();
+  slide[0].style.opacity = "1";
+  for (var j = 0; j < l; j++) {
+    indicator.innerHTML += "<div class='dots' onclick=change(" + j + ")></div>";
+  }
 
-            dots[0].style.background = "#696969";
+  dots[0].style.background = "#696969";
+};
 
-        }
+function initialize() {
+  if (autoplay === "true")
+    set = setInterval(function () {
+      next();
+    }, interval);
+}
 
-        function initialize() {
-            if (autoplay === "true")
-                set = setInterval(function () { next(); }, interval);
-        }
+function change(index) {
+  clearInterval(set);
+  count = index;
+  for (var j = 0; j < l; j++) {
+    slide[j].style.opacity = "0";
+    dots[j].style.background = "#bdbdbd";
+  }
+  slide[count].style.opacity = "1";
+  dots[count].style.background = "#696969";
+}
 
+var count = 0;
+function next() {
+  clearInterval(set);
+  slide[count].style.opacity = "0";
+  count++;
+  for (var j = 0; j < l; j++) {
+    dots[j].style.background = "#bdbdbd";
+  }
 
+  if (count == l) {
+    count = 0;
+    slide[count].style.opacity = "1";
+    dots[count].style.background = "#696969";
+  } else {
+    slide[count].style.opacity = "1";
+    dots[count].style.background = "#696969";
+  }
+  initialize();
+}
 
-        function change(index) {
-            clearInterval(set);
-            count = index;
-            for (var j = 0; j < l; j++) {
-                slide[j].style.opacity = "0";
-                dots[j].style.background = "#bdbdbd";
-            }
-            slide[count].style.opacity = "1";
-            dots[count].style.background = "#696969";
+function prev() {
+  clearInterval(set);
+  slide[count].style.opacity = "0";
+  for (var j = 0; j < l; j++) {
+    dots[j].style.background = "#bdbdbd";
+  }
+  count--;
 
-
-        }
-
-        var count = 0;
-        function next() {
-            clearInterval(set);
-            slide[count].style.opacity = "0";
-            count++;
-            for (var j = 0; j < l; j++) {
-                dots[j].style.background = "#bdbdbd";
-            }
-
-
-            if (count == l) {
-                count = 0;
-                slide[count].style.opacity = "1";
-                dots[count].style.background = "#696969";
-
-            } else {
-                slide[count].style.opacity = "1";
-                dots[count].style.background = "#696969";
-            }
-            initialize()
-        }
-
-
-        function prev() {
-            clearInterval(set);
-            slide[count].style.opacity = "0";
-            for (var j = 0; j < l; j++) {
-                dots[j].style.background = "#bdbdbd";
-            }
-            count--;
-
-            if (count == -1) {
-                count = l - 1;
-                slide[count].style.opacity = "1";
-                dots[count].style.background = "#696969";
-
-            } else {
-                slide[count].style.opacity = "1";
-                dots[count].style.background = "#696969";
-            }
-            initialize()
-        }
+  if (count == -1) {
+    count = l - 1;
+    slide[count].style.opacity = "1";
+    dots[count].style.background = "#696969";
+  } else {
+    slide[count].style.opacity = "1";
+    dots[count].style.background = "#696969";
+  }
+  initialize();
+}
